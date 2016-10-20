c6c9b5aa5641f659787bae8cbb89e7121f934425
boyazhou1993 <boyazhou1993@gmail.com>
2016-10-19 23:07:35 -0400

    commit#6
    add result table and latinSquare

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ $(document).ready(function() {
 	var userList = [];
 	var numQuestion = 6;
 	var originNum = numQuestion;
+	var sys = 0;
+
+	var randomNum = getRandomInt(1,7) - 1;
+
+	var latinSquare = [
+		['F','B','A','C','D','E'],
+		['E','C','B','F','A','D'],
+		['D','A','C','E','F','B'],
+		['A','D','E','B','C','F'],
+		['B','F','D','A','E','C'],
+		['C','E','F','D','B','A']
+	]
+
+	var order = latinSquare[randomNum];
 
 	function createSvg(){
 		$('#warper').prepend('<div id = \'content\'></div>');
@@ -27,7 +41,7 @@ $(document).ready(function() {
 		
 		createSvg();
 		$('#note').remove();
-		var sys = $('#content').myfunction();
+		sys = $('#content').myfunction(order[0]);
 		$('#nextButton').show();
 
 		slider();
@@ -37,6 +51,7 @@ $(document).ready(function() {
 
 		var singleDict = {};
 		singleDict['userSet'] = +$('#percent-display').text();
+		singleDict['sysSet'] = sys;
 		//console.log(singleDict);
 		
 		userList.push(singleDict);
@@ -46,9 +61,11 @@ $(document).ready(function() {
 		if (numQuestion == 1){
 			$('#nextButton').remove();
 			$('#thanks').show();
+			var table = $.makeTable(userList);
+			$(table).appendTo("#thanks");
 		}else{
 			createSvg();
-			$('#content').myfunction();	
+			sys = $('#content').myfunction(order[originNum - numQuestion + 1]);	
 			slider();
 
 			//console.log(+$('#slider').val());
@@ -60,36 +77,38 @@ $(document).ready(function() {
 	console.log(userList);
 
 	//var test_array = [["name1", 2, 3], ["name2", 4, 5], ["name3", 6, 7], ["name4", 8, 9], ["name5", 10, 11]];
-	var fname = "IJGResults";
+	// var fname = "IJGResults";
 
-	var csvContent = "data:text/csv;charset=utf-8,";
-	$("#pressme").click(function(){
-		userList.forEach(function(infoArray, index){
-			dataString = infoArray.join(",");
-			csvContent += index < infoArray.length ? dataString+ "\n" : dataString;
-		});
+	// var csvContent = "data:text/csv;charset=utf-8,";
+	// $("#pressme").click(function(){
+	// 	userList.forEach(function(infoArray, index){
+	// 		dataString = infoArray.join(",");
+	// 		csvContent += index < infoArray.length ? dataString+ "\n" : dataString;
+	// 	});
 
-		var encodedUri = encodeURI(csvContent);
-		window.open(encodedUri);
-	});
+	// 	var encodedUri = encodeURI(csvContent);
+	// 	window.open(encodedUri);
+	//});
 });
 
 
 /* select random chart type tp show */
+//reference : http://hamsterandwheel.com/grids/index2d.php
 (function( $ ){
-   $.fn.myfunction = function() {
+   $.fn.myfunction = function(orderSingle) {
 
-   		var randomInt = getRandomInt(1,4);
-   		if (randomInt ==1){
+   		var randomInt = orderSingle;
+   		
+   		if ((randomInt =='A') || (randomInt =='D')){
    			var sys = makeBar();
    		}
-		else if(randomInt == 2){
+		else if((randomInt =='B') || (randomInt =='E')){
    			var sys = makePie();
    		}
-		else if(randomInt == 3){
+		else if((randomInt =='C') || (randomInt =='F')){
 			var sys = makeRadial();
 		}
-    	return sys;
+    	return Math.round(sys);
    }; 
 })( jQuery );
 
@@ -111,7 +130,23 @@ function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
-
+//reference: http://stackoverflow.com/questions/1051061/convert-json-array-to-an-html-table-in-jquery
+$.makeTable = function (mydata) {
+    var table = $('<table border=1>');
+    var tblHeader = "<tr>";
+    for (var k in mydata[0]) tblHeader += "<th>" + k + "</th>";
+    tblHeader += "</tr>";
+    $(tblHeader).appendTo(table);
+    $.each(mydata, function (index, value) {
+        var TableRow = "<tr>";
+        $.each(value, function (key, val) {
+            TableRow += "<td>" + val + "</td>";
+        });
+        TableRow += "</tr>";
+        $(table).append(TableRow);
+    });
+    return ($(table));
+};
 
 /* make Bar chart */
 function makeBar(){
@@ -142,6 +177,10 @@ function makeBar(){
       var dataChosen = [[dataBar[chosenA], chosenA],
                         [dataBar[chosenB], chosenB]];
 
+      max = d3.max([dataBar[chosenA], dataBar[chosenB]]);
+	  min = d3.min([dataBar[chosenA], dataBar[chosenB]]);
+	  realPercent = (min * 100)/max;
+
       svg.selectAll("rect")
          .data(dataBar)
          .enter()
@@ -162,20 +201,10 @@ function makeBar(){
                     .attr("r", 3)
                     .attr("fill", "#000000");
 
-     return chosenA;
+     return realPercent;
 
-	 /*
 
-	 Real percent: 
-
-	 max = d3.max([dataBar[chosenA], dataBar[chosenB]]);
-	 min = d3.min([dataBar[chosenA], dataBar[chosenB]]);
-	 realPercent = (min * 100)/max;
-
-     */
 };
-
-
 
 /* make Pie chart */
 function makePie(){
@@ -212,6 +241,9 @@ function makePie(){
 	                  .innerRadius(innerRadius)
 	                  .outerRadius(outerRadius);
 	  
+	  max = d3.max([dataPie[chosenA], dataPie[chosenB]]);
+	  min = d3.min([dataPie[chosenA], dataPie[chosenB]]);
+	  realPercent = (min * 100)/max;
 	  // set up groups
 	  var arcs = svg.selectAll("g.arc")
 	                .data(pie(dataPie))
@@ -239,17 +271,7 @@ function makePie(){
 	                .attr("r", 3)
 	                .attr("fill", "#000000");
 
-	  return chosenA;
-
-	 /*
-
-	 Real percent: 
-
-	 max = d3.max([dataPie[chosenA], dataPie[chosenB]]);
-	 min = d3.min([dataPie[chosenA], dataPie[chosenB]]);
-	 realPercent = (min * 100)/max;
-
-     */
+	  return realPercent;
 
 };
 
@@ -258,101 +280,95 @@ function makePie(){
 /* make Radial chart */
 function makeRadial() {
 
-      var width = 900,
-          height = 400,
-          barHeight = height / 2 - 40,
+	  var width = 900,
+	      height = 400,
+	      barHeight = height / 2 - 40,
 		  numBars = 10;
 
-      var formatNumber = d3.format("s");
+	  var formatNumber = d3.format("s");
 
-      var svg = d3.select('svg')
-                  .append("g")
-                  .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+	  var svg = d3.select('svg')
+	              .append("g")
+	              .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
 
-      var values = [];
-      var dataRadial = [{"value":0, "outerRadius":0}, 
-                        {"value":0, "outerRadius":0}, 
-                        {"value":0, "outerRadius":0}, 
-                        {"value":0, "outerRadius":0}, 
-                        {"value":0, "outerRadius":0}, 
-                        {"value":0, "outerRadius":0}, 
-                        {"value":0, "outerRadius":0}, 
-                        {"value":0, "outerRadius":0}, 
-                        {"value":0, "outerRadius":0}, 
-                        {"value":0, "outerRadius":0}];
-                
-      for (var i=0; i<numBars; i++) {
-          var data = Math.random() * (100 - 30) + 30;   // random number (0-100)
-          values.push(data);
-      };
-      dataRadial.forEach(function (d, i) {
-          d.value = values[i];
-      });
-    
+	  var values = [];
+	  var dataRadial = [{"value":0, "outerRadius":0}, 
+	                    {"value":0, "outerRadius":0}, 
+	                    {"value":0, "outerRadius":0}, 
+	                    {"value":0, "outerRadius":0}, 
+	                    {"value":0, "outerRadius":0}, 
+	                    {"value":0, "outerRadius":0}, 
+	                    {"value":0, "outerRadius":0}, 
+	                    {"value":0, "outerRadius":0}, 
+	                    {"value":0, "outerRadius":0}, 
+	                    {"value":0, "outerRadius":0}];
+	            
+	  for (var i=0; i<numBars; i++) {
+	      var data = Math.random() * (100 - 30) + 30;   // random number (0-100)
+	      values.push(data);
+	  };
+	  dataRadial.forEach(function (d, i) {
+	      d.value = values[i];
+	  });
+
 	  /* random picked number */
-      var chosenA = getRandomInt(0, numBars - 1);
-      var chosenB;
-      for (i=0; i<10; i++) {
-          chosenB = getRandomInt(0, numBars - 1);
-          if (chosenB != chosenA) {
-              i = 10;
-          }
-      }
-      var dataChosen = [chosenA, chosenB];
-  
-      var extent = d3.extent(dataRadial, function(d) { return d.value; });
-      var barScale = d3.scale.linear()
-                       .domain(extent)
-                       .range([0, barHeight]);
+	  var chosenA = getRandomInt(0, numBars - 1);
+	  var chosenB;
+	  for (i=0; i<10; i++) {
+	      chosenB = getRandomInt(0, numBars - 1);
+	      if (chosenB != chosenA) {
+	          i = 10;
+	      }
+	  }
+	  var dataChosen = [chosenA, chosenB];
 
-      var arc = d3.svg.arc()
-                  .startAngle(function(d,i) { return (i * 2 * Math.PI) / numBars; })
+	  max = d3.max([dataRadial[chosenA], dataRadial[chosenB]]);
+	  min = d3.min([dataRadial[chosenA], dataRadial[chosenB]]);
+	  realPercent = (min * 100)/max;
+
+	  var extent = d3.extent(dataRadial, function(d) { return d.value; });
+	  var barScale = d3.scale.linear()
+	                   .domain(extent)
+	                   .range([0, barHeight]);
+
+	  var arc = d3.svg.arc()
+	              .startAngle(function(d,i) { return (i * 2 * Math.PI) / numBars; })
 				  .endAngle(function(d,i) { return ((i + 1) * 2 * Math.PI) / numBars; })
 				  .innerRadius(0);
 
-      var segments = svg.selectAll("path")
-                        .data(dataRadial)
-                        .enter()
-                        .append("path")
+	  var segments = svg.selectAll("path")
+	                    .data(dataRadial)
+	                    .enter()
+	                    .append("path")
 						.style("fill", "#ffffff")
 						.style("stroke", "#000000")
 						.attr("d", function(d, index) {
 							d.outerRadius = d.value * 2;
 							return arc(d, index);
-                        });
-          
-      // Marks
-      var markRadius = barHeight - 140;
+	                    });
+	      
+	  // Marks
+	  var markRadius = barHeight - 140;
 
-      var marks = svg.append("g")
-                     .classed("marks", true)
+	  var marks = svg.append("g")
+	                 .classed("marks", true)
 					 .append("def")
-                     .append("path")
-                     .attr("id", "label-path")
+	                 .append("path")
+	                 .attr("id", "label-path")
 					 .attr("d", "m0 " + -markRadius + " a" + markRadius + " " + markRadius + " 0 1,1 -0.01 0");
 
-      marks.selectAll("text")
-           .data(dataChosen)
-           .enter()
+	  marks.selectAll("text")
+	       .data(dataChosen)
+	       .enter()
 		   .append("text")
-           .style("text-anchor", "middle")
-           .append("textPath")
-           .attr("xlink:href", "#label-path")
-           .attr("startOffset", function(d, i) {return d * 100 / numBars + 50 / numBars + '%';})
-          .style("font-weight","bold")
-          .text("·");
+	       .style("text-anchor", "middle")
+	       .append("textPath")
+	       .attr("xlink:href", "#label-path")
+	       .attr("startOffset", function(d, i) {return d * 100 / numBars + 50 / numBars + '%';})
+	      .style("font-weight","bold")
+	      .text("·");
 
-	  return chosenA;
+	return chosenA;
 
 
-	 /*
-
-	 Real percent: 
-
-	 max = d3.max([values[chosenA], values[chosenB]]);
-	 min = d3.min([values[chosenA], values[chosenB]]);
-	 realPercent = (min * 100)/max;
-
-     */
-
-}
+};
