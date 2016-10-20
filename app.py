0f17c03b84971752a6b432dfb21757fb6948b2e7
boyazhou1993 <boyazhou1993@gmail.com>
2016-10-20 19:53:53 -0400

    commit#8
    add simple server

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import json
+import pandas as pd
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/store_data', methods=['GET', 'POST'])
+def store_data():
+	user_list = request.args.get('user_list')
+	user_list = json.loads(user_list)
+	df = pd.DataFrame(user_list)
+	t = time.time()
+	df.to_csv(str(t))
+	print(user_list)
+	return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
