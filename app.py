b998849432bf4a317077b85b02d66a179690360e
boyazhou1993 <boyazhou1993@gmail.com>
2016-10-21 18:28:29 -0400

    fix detail problem

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ from flask import Flask, render_template, request
 import json
 import pandas as pd
 import time
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/store_data', methods=['GET', 'POST'])
+@app.route('/store_data', methods=['GET'])
 def store_data():
 	user_list = request.args.get('user_list')
 	user_list = json.loads(user_list)
